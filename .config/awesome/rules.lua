1a73a15b1ec938335bbae31112dc9141884b92da
Damir Garifullin <gosugdr@gmail.com>
2019-05-18 18:47:01 +0300

    [AwesomeWM] Added alacirtty rule
    Problem: No rule for alacritty
    
    Solution: Added rule for alacritty term emulator to AwesomeWM config

--- .config/awesome/rules.lua
+++ .config/awesome/rules.lua
@@ -50,6 +50,16 @@ awful.rules.rules = {
     }
   }, properties = { floating = true } },
 
+  {
+    rule = {
+      class = "Alacritty",
+    },
+    properties = {
+      tag = "1",
+      opacity = "0.8",
+    },
+  },
+
   -- Set Firefox to always map on the tag named "2" on screen 1.
   -- { rule = { class = "Firefox" },
   --   properties = { screen = 1, tag = "2" } },
