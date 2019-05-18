e429689335545e7265379a319b7934230b186d56
Damir Garifullin <gosugdr@gmail.com>
2019-05-18 18:40:02 +0300

    [AwesomeWM] Disabled client focus on mouse move
    Problem: AwesomeWM client focus follows mouse
    
    Solution: Focus changes on mouse click

--- .config/awesome/rules.lua
+++ .config/awesome/rules.lua
@@ -112,7 +112,7 @@ client.connect_signal("request::titlebars", function(c)
 end)
 
 -- Enable sloppy focus, so that focus follows mouse.
-client.connect_signal("mouse::enter", function(c)
+client.connect_signal("mouse::click", function(c)
   c:emit_signal("request::activate", "mouse_enter", { raise = false })
 end)
 
