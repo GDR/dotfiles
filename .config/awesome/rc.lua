cf978c59a06ae4d0dd1b3c286aa56690a2e535a9
Damir Garifullin <gosugdr@gmail.com>
2019-05-18 22:26:26 +0300

    [AwesomeWM] Created local theme
    Problem: Themes located in default folder
    
    Solution: Moved themes to .config/awesome folder. Now rc.lua takes theme from local folder

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -1,9 +1,12 @@
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
-
+local beautiful = require("beautiful")
+local gears = require("gears")
 -- Standard awesome library
 require("awful.autofocus")
+
+beautiful.init(gears.filesystem.get_configuration_dir() .. "/theme/gtk/theme.lua")
 
 require("./notifications")
 require("./layouts")
